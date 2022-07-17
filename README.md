3394dfc13249ee404ab582b0f8cd54df0f5d176c
vans <spentmny@nate.com>
2022-07-17 23:11:31 +0900

    Chore: 프로젝트 초기 셋팅

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MUSINSA_Assignment
+
 무신사 과제 제출을 위한 Repository입니다.
---
+++ src/main/java/com/example/musinsa_assignment/MusinsaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.musinsa_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusinsaAssignmentApplication.class, args);
+	}
+
+}
